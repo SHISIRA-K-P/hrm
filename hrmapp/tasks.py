d8a3ad51936797e861c8717a4201f69259c35563
SHISIRA-K-P <shisirakrishna@gmail.com>
2022-08-18 12:36:34 +0530

    changeoccurred

--- hrmapp/tasks.py
+++ hrmapp/tasks.py
@@ -1,22 +1,7 @@
-# from celery.decorators import task
-# from celery.utils.log import get_task_logger
-# from django.core.management import call_command
 from celery import shared_task
 from celery import app as celery_app
-# from django.core.mail import EmailMessage
-
 from django.core.mail import send_mail
 from hrm import settings
-
-# logger = get_task_logger(__name__)
-
-
-# @shared_task
-# def test():
-#     logger.info('test')
-
-
-
 
 @shared_task(name='send_email_task')
 def send_maildetails(email):
@@ -30,13 +15,4 @@ def send_maildetails(email):
 
 
 
-# @app.task(name='test')
-# def test():
-#     logger.info('testtttttttt')
-
-
-# send_mail_task.delay(your_subject, your_messaage, your_email, your_recepient_list)
-
-# method2
-# from django.core.mail import send_mail
 

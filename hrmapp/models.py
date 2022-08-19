fab6e262c7927111948d04692dc283e1f5fdd7a9
SHISIRA-K-P <shisirakrishna@gmail.com>
2022-08-19 14:14:26 +0530

    pep8 std applied

--- hrmapp/models.py
+++ hrmapp/models.py
@@ -2,21 +2,20 @@ from django.db import models
 from authentication.models import User
 import datetime
 # Create your models here.
+# create Leave model
 
-#create Leave model
+
 class Leave(models.Model):
-    
     date_from = models.DateField(default=datetime.date.today)
     date_to = models.DateField(default=datetime.date.today)
-    leave_status= (
+    leave_status = (
         ('select', 'select'),
         ('pending', 'pending'),
         ('approved', 'approved'),
         ('cancel', 'cancel'),
-         ('reject', 'reject'),)
-      
-    status = models.CharField(choices=leave_status,default="select",max_length=20)
-    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True) 
+        ('reject', 'reject'), )
+    status = models.CharField(choices=leave_status, default="select", max_length=20)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True) 
 
     def _str__(self):
         return self.status
@@ -27,8 +26,7 @@ class Leave(models.Model):
 class Feedback(models.Model):
     
     content = models.CharField(max_length=250)
-    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def _str__(self):
         return self.content
-    
